0c86bff5412460d7f9eec8e41d1d8f8b5835bbda
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-04-17 23:30:20 +0900

    fix for eslint

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,17 @@
 import { initializeWrappedStore } from '../app/store';
 
 initializeWrappedStore();
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'translation',
+    title: '選択したテキストを翻訳',
+    contexts: ['selection'],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === 'translation') {
+    alert('翻訳します');
+  }
+});
