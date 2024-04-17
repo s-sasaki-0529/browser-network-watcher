655c462f734f4d59dc8c0bd63880eb778f7cd1ba
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-04-17 21:56:01 +0900

    fiefox 専用のコード分岐を削除

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -3,8 +3,7 @@ import 'construct-style-sheets-polyfill';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { debounce } from 'lodash-es';
-import { twind, config, cssom, observe, stringify } from './twind';
+import { twind, config, cssom, observe } from './twind';
 import { proxyStore } from '../app/proxyStore';
 import Content from './Content';
 
@@ -17,23 +16,7 @@ proxyStore.ready().then(() => {
   const shadowRoot = contentRoot.attachShadow({ mode: 'open' });
   const sheet = cssom(new CSSStyleSheet());
 
-  // shadowRoot.adoptedStyleSheet bug in firefox
-  // see: https://bugzilla.mozilla.org/show_bug.cgi?id=1827104
-  if (navigator?.userAgent.includes('Firefox')) {
-    const style = document.createElement('style');
-    const debouncedSyncCss = debounce(() => {
-      style.textContent += stringify(sheet.target);
-    }, 100);
-
-    const originalSheetInsert = sheet.insert;
-    (sheet.insert as typeof originalSheetInsert) = (...params) => {
-      originalSheetInsert(...params);
-      debouncedSyncCss();
-    };
-    shadowRoot.appendChild(style);
-  } else {
-    shadowRoot.adoptedStyleSheets = [sheet.target];
-  }
+  shadowRoot.adoptedStyleSheets = [sheet.target];
 
   const tw = twind(config, sheet);
   observe(tw, shadowRoot);
