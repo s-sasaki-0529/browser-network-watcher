9d0a30baa184c2cf32f779fa43c013cc0edde79a
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-04-17 22:17:27 +0900

    smarthr-ui を導入

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,13 +3,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { proxyStore } from '../app/proxyStore';
+import 'smarthr-ui/smarthr-ui.css';
+import { createTheme, ThemeProvider } from 'smarthr-ui';
 import Popup from './Popup';
+
+const theme = createTheme();
 
 proxyStore.ready().then(() => {
   createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
       <Provider store={proxyStore}>
-        <Popup />
+        <ThemeProvider theme={theme}>
+          <Popup />
+        </ThemeProvider>
       </Provider>
     </React.StrictMode>
   );
