b38ad8a72338bbc97521a2af506b0afcd5444a51
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-04-20 23:20:56 +0900

    リクエスト内容を画面上に表示するようにする

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-  const requestUrl = new URL(details.url)
-  const currentUrl = new URL(details.initiator || "")
-  if (requestUrl.hostname === currentUrl.hostname && requestUrl.href.includes('/api/')) {
-    console.log(requestUrl.href.split('/api/')[1])
+function main() {
+  chrome.webRequest.onBeforeRequest.addListener((details) => {
+    const requestUrl = new URL(details.url)
+    const currentUrl = new URL(details.initiator || "")
+    if (requestUrl.hostname === currentUrl.hostname && requestUrl.href.includes('/api/')) {
+      const apiPath = requestUrl.href.split('/api/')[1]
+      chrome.tabs.sendMessage(details.tabId, { text: apiPath})
+    }
+    return {};
+  }, { urls: ['<all_urls>'] })
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    main()
   }
-  return {};
-}, { urls: ['<all_urls>'] })
+})
---
+++ contentScript.js
@@ -0,0 +1,18 @@
+// オーバーレイ表示用のDOMを追加する
+const overlay = document.createElement('div');
+overlay.style.position = 'fixed';
+overlay.style.bottom = '10px';
+overlay.style.right = '10px';
+overlay.style.backgroundColor = 'white';
+overlay.style.border = '1px solid black';
+overlay.style.padding = '5px';
+overlay.style.zIndex = '1000';
+overlay.innerText = 'Hello, world!';
+document.body.appendChild(overlay);
+
+// background から受け取っったメッセージを描画する
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.text) {
+    overlay.innerText = message.text;
+  }
+});
