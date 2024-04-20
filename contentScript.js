0fac3d2dc78485a58948504605f89f963a3fc446
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-04-20 23:31:56 +0900

    fix for Prettier

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,12 @@
 // オーバーレイ表示用のDOMを追加する
-const overlay = document.createElement('div');
-overlay.style.position = 'fixed';
-overlay.style.bottom = '10px';
-overlay.style.right = '10px';
-overlay.style.backgroundColor = 'white';
-overlay.style.border = '1px solid black';
-overlay.style.padding = '5px';
-overlay.style.zIndex = '1000';
+const overlay = document.createElement("div");
+overlay.style.position = "fixed";
+overlay.style.bottom = "10px";
+overlay.style.right = "10px";
+overlay.style.backgroundColor = "white";
+overlay.style.border = "1px solid black";
+overlay.style.padding = "5px";
+overlay.style.zIndex = "1000";
 document.body.appendChild(overlay);
 
 // background から受け取っったメッセージを描画する
