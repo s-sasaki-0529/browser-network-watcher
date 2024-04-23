20c55324f03683e317b58957dfc558f4907c6b41
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-04-23 23:03:12 +0900

    移動ボタンのレイアウトを調整する　

--- src/content/components/RequestListContainer.tsx
+++ src/content/components/RequestListContainer.tsx
@@ -10,8 +10,8 @@ export const RequestListContainer: React.FC<Props> = (_props) => {
   const elRef = useRef<HTMLDivElement>(null);
   const [tabId, setTabId] = useState<number>(0);
   const [requestList, setRequestList] = useState<AnyRequest[]>([]);
-  const [isLeftPosition, setIsLeftPosition] = useState(true);
-  const [isTopPosition, setIsTopPosition] = useState(true);
+  const [isLeftPosition, setIsLeftPosition] = useState(false);
+  const [isTopPosition, setIsTopPosition] = useState(false);
 
   /**
    * バックグラウンドからのメッセージとステートを同期する
@@ -35,32 +35,32 @@ export const RequestListContainer: React.FC<Props> = (_props) => {
     }
   }, [requestList]);
 
-  if (requestList.length === 0) return null;
-
   return (
-    <StyledRootDiv ref={elRef} isLeftPosition={isLeftPosition} isTopPosition={isTopPosition}>
-      <ul>
-        {requestList.map((req) => (
-          <RequestListItem
-            request={req}
-            key={req.id}
-            onClickRequest={() => {
-              chrome.runtime.sendMessage({ type: "onClickRequest", tabId, value: req });
-            }}
-          />
-        ))}
-      </ul>
-      <MoveButtonWrapper>
-        <MoveButton onClick={() => setIsLeftPosition(true)}>←</MoveButton>
-        <MoveButton onClick={() => setIsTopPosition(true)}>↑</MoveButton>
-        <MoveButton onClick={() => setIsLeftPosition(false)}>→</MoveButton>
-        <MoveButton onClick={() => setIsTopPosition(false)}>↓</MoveButton>
-      </MoveButtonWrapper>
-    </StyledRootDiv>
+    <RootDiv ref={elRef} isLeftPosition={isLeftPosition} isTopPosition={isTopPosition}>
+      <RequestListWrapper>
+        <ul>
+          {requestList.map((req) => (
+            <RequestListItem
+              request={req}
+              key={req.id}
+              onClickRequest={() => {
+                chrome.runtime.sendMessage({ type: "onClickRequest", tabId, value: req });
+              }}
+            />
+          ))}
+        </ul>
+      </RequestListWrapper>
+      <MenuButtonWrapper>
+        <MenuButton onClick={() => setIsLeftPosition(true)}>←</MenuButton>
+        <MenuButton onClick={() => setIsTopPosition(true)}>↑</MenuButton>
+        <MenuButton onClick={() => setIsLeftPosition(false)}>→</MenuButton>
+        <MenuButton onClick={() => setIsTopPosition(false)}>↓</MenuButton>
+      </MenuButtonWrapper>
+    </RootDiv>
   );
 };
 
-const StyledRootDiv = styled.div<{ isLeftPosition: boolean; isTopPosition: boolean }>`
+const RootDiv = styled.div<{ isLeftPosition: boolean; isTopPosition: boolean }>`
   position: fixed;
   top: ${(props) => (props.isTopPosition ? "0" : "auto")};
   bottom: ${(props) => (props.isTopPosition ? "auto" : "0")};
@@ -70,12 +70,20 @@ const StyledRootDiv = styled.div<{ isLeftPosition: boolean; isTopPosition: boole
   border: 1px solid black;
   padding: 5px;
   width: 25vw;
-  max-height: 15vh;
-  overflow-y: auto;
+  height: 15vh;
   z-index: 2147483004;
 `;
 
-const MoveButtonWrapper = styled.div`
+const RequestListWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+`;
+
+const MenuButtonWrapper = styled.div`
+  position: absolute;
+  right: 1.5rem;
+  bottom: 0.5rem;
   display: flex;
   gap: 0.5rem;
   justify-content: flex-end;
@@ -83,11 +91,8 @@ const MoveButtonWrapper = styled.div`
   height: 0;
 `;
 
-const MoveButton = styled.button`
+const MenuButton = styled.button`
   background-color: transparent;
   border: none;
   cursor: pointer;
-  &:hover {
-    background-color: #888;
-  }
 `;
