1a6f7423ff15bfed8dd6891c4feb4078b34ba2f2
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-04-29 22:42:00 +0900

    refactor: タブの初期化状態とリクエスト一覧を同じオブジェクトで管理するように

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,34 +2,41 @@ import { completeRequestInfo, isValidRequest, newReuqestInfo } from "../lib/requ
 import type { AnyRequest } from "../lib/request.js";
 import { MessageToBackground, MessageToContent, TabId } from "../lib/types.js";
 
-// 初期化が完了したタブ一覧
-const initializedTabs: TabId[] = [];
-
-// タブごとのリクエスト一覧
-const requestList: Record<TabId, AnyRequest[]> = {};
+// タブ一覧
+const tabs: {
+  [tabId: number]: {
+    requestList: AnyRequest[];
+    origin: string;
+  };
+} = {};
 
 // コンテントとリクエスト一覧を同期する
 const sendUpdateRequestListMessage = (tabId: TabId) => {
-  chrome.tabs.sendMessage<MessageToContent>(tabId, { tabId, type: "updateRequestList", value: requestList[tabId] });
+  chrome.tabs.sendMessage<MessageToContent>(tabId, {
+    tabId,
+    type: "updateRequestList",
+    value: tabs[tabId].requestList,
+  });
+  console.log(tabs);
 };
-
-//
-chrome.webNavigation.onCompleted.addListener((details) => {
-  requestList[details.tabId] = [];
-});
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // タブの初期化
-  if (initializedTabs.includes(tabId)) return;
-  initializedTabs.push(tabId);
-  requestList[tabId] = [];
+  if (tabs[tabId] !== undefined) return;
+  tabs[tabId] = {
+    requestList: [],
+    origin: "",
+  };
 
   // リクエスト発生イベント
   // リクエスト一覧に新しいリクエストオブジェクトを追加し、コンテンツスクリプトに送信
   chrome.webRequest.onBeforeRequest.addListener(
     (details) => {
-      if (isValidRequest(details) && requestList[tabId].find((req) => req.id === details.requestId) === undefined) {
-        requestList[tabId].push(newReuqestInfo(details));
+      if (
+        isValidRequest(details) &&
+        tabs[tabId].requestList.find((req) => req.id === details.requestId) === undefined
+      ) {
+        tabs[tabId].requestList.push(newReuqestInfo(details));
         sendUpdateRequestListMessage(tabId);
       }
       return {};
@@ -41,7 +48,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // リクエストオブジェクトの情報を更新し、コンテンツスクリプトに送信
   chrome.webRequest.onCompleted.addListener(
     (details) => {
-      completeRequestInfo(requestList[tabId], details);
+      completeRequestInfo(tabs[tabId].requestList, details);
       sendUpdateRequestListMessage(tabId);
       return {};
     },
